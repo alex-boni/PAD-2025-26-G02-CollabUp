02ebe2de5eaee89ab127bedd6df6f04c6411dcc7
Rubén <ruben200409@gmail.com>
2025-10-24 14:37:02 +0200

    Añadida vist ajustes a CollabView abstracto y añadidos metodos a la interfaz
    Co-authored-by: rouu04 <146331236+rouu04@users.noreply.github.com>

--- app/src/main/java/es/ucm/fdi/pad/collabup/modelo/collabView/CollabView.java
+++ app/src/main/java/es/ucm/fdi/pad/collabup/modelo/collabView/CollabView.java
@@ -1,6 +1,14 @@
 package es.ucm.fdi.pad.collabup.modelo.collabView;
 
+import android.app.Activity;
+
+import androidx.fragment.app.Fragment;
+
 //Interfaz collabViews
 public interface CollabView {
+    Activity getVistaCollabView();
+    Fragment getFragmentEnCollab();
+    Activity getVistaAjustes();
     AbstractCollabView construir();
+    //TODO Añadir getMiniatura
 }
