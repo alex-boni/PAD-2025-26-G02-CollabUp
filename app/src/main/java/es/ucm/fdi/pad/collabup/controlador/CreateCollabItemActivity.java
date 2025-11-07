7ec4aecf523c222e40701dd14594f68a245dfb20
rouu04 <rociunon@ucm.es>
2025-11-07 13:55:36 +0100

    Reorganizar código para que las funciones que llaman a base de datos se hagan en una clase a parte

--- app/src/main/java/es/ucm/fdi/pad/collabup/controlador/CreateCollabItemActivity.java
+++ app/src/main/java/es/ucm/fdi/pad/collabup/controlador/CreateCollabItemActivity.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.Timestamp;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,25 +19,27 @@ import java.util.Locale;
 import es.ucm.fdi.pad.collabup.R;
 import es.ucm.fdi.pad.collabup.modelo.Etiqueta;
 import es.ucm.fdi.pad.collabup.modelo.collabView.CollabItem;
-import es.ucm.fdi.pad.collabup.modelo.interfaz.OnDataLoadedCallback;
+import es.ucm.fdi.pad.collabup.modelo.collabView.DAOCollabItemImp;
+import es.ucm.fdi.pad.collabup.modelo.interfaz.OnOperationCallback;
 
 //Llamado cuando vayamos a añadir evento
 public class CreateCollabItemActivity extends AppCompatActivity {
-
-    //Atributos base de datos
-    private FirebaseFirestore db;
 
     // ---------Atributos vista
     private EditText eTxtNombreCollabItem, eTxtDescripcionCollabItem, eTxtFechaCollabItem;
     private Button btnCrearCollabItem;
 
+    //---------- Atributos necesarios
     private String idC; //id del collab en el que estamos
+    DAOCollabItemImp daoci;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_collabitem);
+
+        daoci = new DAOCollabItemImp();
 
         // Inicializar vistas
         eTxtNombreCollabItem = findViewById(R.id.eTxtNombreCollabItem);
@@ -53,8 +54,6 @@ public class CreateCollabItemActivity extends AppCompatActivity {
         }
         //todo faltan
 
-        // Inicializar Firebase
-        db = FirebaseFirestore.getInstance();
 
         // Configurar listener del botón
         btnCrearCollabItem.setOnClickListener(new View.OnClickListener() {
@@ -63,10 +62,6 @@ public class CreateCollabItemActivity extends AppCompatActivity {
                 crearNuevoCollabItem();
             }
         });
-    }
-
-    public void obtener(String identificador, OnDataLoadedCallback<CollabItem> callback) {
-
     }
 
     //Crear item en collab
@@ -97,18 +92,19 @@ public class CreateCollabItemActivity extends AppCompatActivity {
         // Creamos el objeto CollabItem
         CollabItem nuevoCollabItem = new CollabItem(nombre, descripcion, fecha, uasig, easig, idC);
 
-        // Guardar en Firestore en la colección "collabItem" dentro de su "collabs"
-        // .add() crea un documento con un ID automático
-        db.collection("collabs")
-                .document(idC)
-                .collection("collabItem")
-                .add(nuevoCollabItem)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(CreateCollabItemActivity.this, "CollabItem creado con éxito", Toast.LENGTH_SHORT).show();
-                    //nuevoCollabItem.setIdI(documentReference.getId()); así se cogería el id
-                    finish(); // Cierra la actividad y vuelve al fragmento
-                })
-                .addOnFailureListener(e -> Toast.makeText(CreateCollabItemActivity.this, "Error al crear: " + e.getMessage(), Toast.LENGTH_LONG).show());
+        //Lo añadimos a la base de datos
+        daoci.crear(nuevoCollabItem, new OnOperationCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(CreateCollabItemActivity.this, "CollabItem creado con éxito", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(CreateCollabItemActivity.this, "Error al crear: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
